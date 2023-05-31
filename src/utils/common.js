a98a3445e884fde3abdb31774ce7e19e1b0f32ee
vlad1slove1 <vlad5114@gmail.com>
2023-05-31 15:49:23 +0300

    fix fillArrWithTickets

--- src/utils/common.js
+++ src/utils/common.js
@@ -10,7 +10,21 @@ const fillArrWithTickets = (res, arr) => {
     const uniqueTicketId = Object.values(item)[1];
     const uniqueTicketNumber = Object.values(item)[6];
 
-    arr.push(`Код ${uniqueTicketId}, id купона: ${uniqueTicketNumber}`);
+    /**
+     * Так как telegram на своей стороне парсит все тексты и ищет в них
+     * номера телефонов, ссылки, локации, номера банковских карт, то приходится
+     * обработать нашу строку и исключить все совпадения
+     *
+     * Для этого используется спец. символ нулевой ширины, который не будет
+     * отображаться в финальном выводе строки
+     */
+    const hiddenChar = '\u200B'; // Символ нулевой ширины
+    const middleIndex = Math.floor(uniqueTicketId.length / 2); // Делим строку пополам
+    const firstHalf = uniqueTicketId.slice(0, middleIndex);
+    const secondHalf = uniqueTicketId.slice(middleIndex);
+    const protectedTicketId = [firstHalf, hiddenChar, secondHalf].join('');
+
+    arr.push(`Код ${protectedTicketId}, id купона: ${uniqueTicketNumber}`);
   });
 };
 
