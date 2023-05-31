59275679f2d0018f58bc1b4a3849a5d47fed2c3b
vlad1slove1 <vlad5114@gmail.com>
2023-05-31 15:48:58 +0300

    add cron-shedule task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { config } from 'dotenv';
+import cron from 'node-cron';
 
 import state from '../states/index.js';
+import syncSheetsToMongo from './utils/syncSheetsToMongo.js';
 
 const { PORT } = process.env;
 
@@ -14,9 +16,14 @@ const app = express();
 // state.dev();
 
 // draw - идёт розыгрыш
-// state.draw();
+state.draw();
 
 // idle - розыгрыш закончился ("заглушка")
-state.idle();
+// state.idle();
+
+// Синхронизируем таблицу с базой данных каждый день в 6:00, 12:00, 18:00 и 23:59
+cron.schedule('0 0 6,12,18,23 * * *', () => {
+  syncSheetsToMongo();
+});
 
 app.listen(PORT);
